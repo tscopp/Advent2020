d3ae24014d18687e681d09473a321aa3feeeae48
tms <scoppettat@gmail.com>
2020-12-01 16:01:31 -0800

    removed print

--- 1/1.py
+++ 1/1.py
@@ -1,7 +1,6 @@
 def expensify():
   expenses = open('input', 'r').readlines()
   expenses = [expense.strip() for expense in expenses]
-  print(expenses)
   for expense in expenses:
     for compare_expense in expenses:
       if int(expense) + int(compare_expense) == 2020:
