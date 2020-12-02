877b3e19fc3461a76660d3ec0f6966e56df6ace0
tms <scoppettat@gmail.com>
2020-12-01 16:03:34 -0800

    one lined expense comprehension

--- 1/1.py
+++ 1/1.py
@@ -1,6 +1,5 @@
 def expensify():
-  expenses = open('input', 'r').readlines()
-  expenses = [expense.strip() for expense in expenses]
+  expenses = [expense.strip() for expense in open('input', 'r').readlines()]
   for expense in expenses:
     for compare_expense in expenses:
       if int(expense) + int(compare_expense) == 2020:
